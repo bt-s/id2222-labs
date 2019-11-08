b2b2cfcc549be2e9d5b4a4a446e0ac1483638648
Bas Straathof <bas_straathof@msn.com>
2019-11-08 09:24:21 +0100

    Parse CL arguments in main.py

--- lab1/main.py
+++ lab1/main.py
@@ -8,73 +8,82 @@ __author__ = "Xenia Ioannidou and Bas Straathof"
 
 
 import pandas as pd
+from argparse import ArgumentParser, Namespace
+from sys import argv
 
 from helpers import load_dataset, compare_sets
 from shingles import Shingles
 from min_hashing import MinHash
 from lsh import LSH
 
-# To use the very simple toy data set
-TOY_TOY_DATASET = False
 
-SHINGLES = False
-MINHASH = False
-USE_LSH = True
+def parse_args():
+    """Parses CL arguments"""
+    parser = ArgumentParser()
 
-if TOY_TOY_DATASET:
-    X = load_dataset("toy_toy_dataset.csv")
-else:
-    X = load_dataset("toy_dataset.csv")
+    parser.add_argument("-d", "--dataset", type=str, default="toy_dataset.csv")
+    parser.add_argument("-t", "--task", type=str, default="shingles",
+            help="The task that should be carried out: shingles, minhash or "
+            "lsh.")
 
-# Sanity-check that X is a pandas DataFrame
-assert type(X) == pd.DataFrame
+    return parser.parse_args(argv[1:])
 
 
-if SHINGLES:
-    # Define the Shingles object
-    obj = Shingles()
+def main(args: Namespace):
+    """Main function for lab 1
 
-    # Create a shingles dataframe based on the data
-    df_shingles = obj.create_shingles(X)
+    Args:
+        args: CL arguments
+    """
+    X = load_dataset(args.dataset)
 
-    # Create sets of shingles for the first two documents
-    set_d1 = set(df_shingles.iloc[0].to_list()[0])
-    set_d2 = set(df_shingles.iloc[1].to_list()[0])
+    if args.task == "shingles":
+        # Define the Shingles object
+        obj = Shingles()
 
-    # Compare the hash-shingled documents using Jaccard similarity
-    obj.compare_sets(set_d1, set_d1)
+        # Create a shingles dataframe based on the data
+        df_shingles = obj.create_shingles(X)
+
+        # Create sets of shingles for the first two documents
+        set_d1 = set(df_shingles.iloc[0].to_list()[0])
+        set_d2 = set(df_shingles.iloc[1].to_list()[0])
+
+        # Compare the hash-shingled documents using Jaccard similarity
+        obj.compare_sets(set_d1, set_d1)
 
 
-if MINHASH:
-    # Define the MinHash object
-    obj = MinHash()
+    if args.task == "minhash":
+        # Define the MinHash object
+        obj = MinHash()
 
-    # Create the characteristic matrix
-    M = obj.create_shingles(X)
+        # Create the characteristic matrix
+        M = obj.create_shingles(X)
 
-    # Create the signature  matrix
-    S = obj.create_signatures(M, k=1000)
-    print(f"The approximated Jaccard similarity between document d1 and d2 is: "
-            f"{compare_sets(S, 0, 1)}")
+        # Create the signature  matrix
+        S = obj.create_signatures(M, k=1000)
+        print(f"The approximated Jaccard similarity between document d1 and d2 is: "
+                f"{compare_sets(S, 0, 1)}")
 
-if USE_LSH:
-    # Define the MinHash object
-    mh = MinHash()
+    if args.task == "lsh":
+        # Define the MinHash object
+        mh = MinHash()
 
-    # Create the characteristic matrix
-    M = mh.create_shingles(X)
+        # Create the characteristic matrix
+        M = mh.create_shingles(X)
 
-    # Create the signature  matrix
-    S = mh.create_signatures(M, k=1000)
+        # Create the signature  matrix
+        S = mh.create_signatures(M, k=1000)
 
-    # Define the LSH object
-    lsh = LSH(S)
+        # Define the LSH object
+        lsh = LSH(S)
 
-    #print(f"The approximated Jaccard similarity between document d1 and d2 is: "
-            #f"{compare_sets(S, 0, 1)}")
+        #print(f"The approximated Jaccard similarity between document d1 and d2 is: "
+                #f"{compare_sets(S, 0, 1)}")
 
-    pairs = lsh.find_candidate_pairs(S, bands=50)
-    pairs = lsh.compare_pairs(S, pairs)
-    print(pairs)
+        pairs = lsh.find_candidate_pairs(S, bands=50)
+        pairs = lsh.compare_pairs(S, pairs)
+        print(pairs)
 
+if __name__ == "__main__":
+    main(parse_args())
 
