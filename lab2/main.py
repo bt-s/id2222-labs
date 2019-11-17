ff6f4fe1c1c7d12e8a2351cd1d6c281646f17ece
Bas Straathof <bas_straathof@msn.com>
2019-11-17 17:39:39 +0100

    Print more information in main.py

--- lab2/main.py
+++ lab2/main.py
@@ -19,8 +19,8 @@ def parse_args():
     """Parses CL arguments"""
     parser = ArgumentParser()
 
-    parser.add_argument("-d", "--dataset", type=str, default="./toy.csv")
-    parser.add_argument("-s", "--support", type=int, default=2)
+    parser.add_argument("-d", "--dataset", type=str, default="./T10I4D100K.dat")
+    parser.add_argument("-s", "--support", type=int, default=1000)
     parser.add_argument("-c", "--confidence", type=float, default=0.5)
     parser.add_argument("-k", "--k", type=int, default=10)
 
@@ -28,16 +28,23 @@ def parse_args():
 
 
 def main(args: Namespace):
-
     try:
         df = load_dataset(args.dataset)
         f_item_sets = apriori(df, args.support, args.k)
-        print("The frequent item sets are:")
-        print(f_item_sets)
+        lengths = {}
+        for k,v in f_item_sets.items():
+            if len(k) in lengths:
+                lengths[len(k)] +=1
+            else:
+                lengths[len(k)] = 1
+        print(f"The frequent itemsets are: {f_item_sets}\n")
+        print(f"The distribution of frequent itemsets is: {lengths}.\n")
         a_rules = association_rules(f_item_sets, args.confidence)
         print("\nThe association rules are:")
         for rule in a_rules.items():
             print(rule)
+
+        print(f"\nA total of {len(a_rules)} association rules was found.")
 
     except FileNotFoundError:
         print("File does not exist")
