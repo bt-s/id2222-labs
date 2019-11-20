9b0327b9729629b73019a09f47d40ac51ade94e8
Bas Straathof <bas_straathof@msn.com>
2019-11-20 23:49:27 +0100

    Write docstrings for helpers

--- lab3/helpers.py
+++ lab3/helpers.py
@@ -13,34 +13,64 @@ from networkx.drawing.nx_pylab import draw_networkx
 import matplotlib.pyplot as plt
 
 
-def create_graph(f_name: str, n=2, sep=" ") -> Graph:
-    Data = open(f_name, "r")
+def create_graph(f_name: str, n: int =2, sep: str =" ") -> Graph:
+    """Creates a graph from a file
+
+    Args:
+        f_name: The filename/path
+        n: The number of lines on the top of the file to skip
+        sep: The value separator in the file
+
+    Returns:
+        G: The graph
+    """
+    data = open(f_name, "r")
     for i in range(n):
-        next(Data, None) # Delete a line in the input file
+        next(data, None) # Delete a line in the input file
 
     # Define the graph type
     Graphtype = nx.Graph()
 
     # Define the graph
-    G = nx.parse_edgelist(Data, delimiter=sep, create_using=Graphtype,
+    G = nx.parse_edgelist(data, delimiter=sep, create_using=Graphtype,
             nodetype=int)
 
     return G
 
 
-def draw_graph(G, labels=True):
+def draw_graph(G: Graph, labels: bool=True, save: bool=False, fname: str="test.pdf"):
+    """Draw a graph structure
+
+    Args:
+        G: The graph to be drawn
+        labels: Whether to draw the labels
+        save: Whetehr to save the file
+        fname: The path and name of the file to be saved
+    """
     draw_networkx(G, with_labels=labels, node_color="lightblue")
-    plt.show()
+
+    if save:
+        plt.savefig(fname)
+    else:
+        plt.show()
+
+    plt.close()
 
 
 def get_graph_statistics(G):
-    print(f"\nThe number of edges is: {len(G.edges)}")
-    print(f"\nThe number of nodes is: {len(G.nodes)}")
+    """Find the edges, nodes and triangles in a graph
+
+    Args:
+        G: The graph to be analyzed
+    """
+    print(f"The number of edges is: {len(G.edges)}")
+    print(f"The number of nodes is: {len(G.nodes)}")
 
     # Find all cliques
     all_cliques = nx.enumerate_all_cliques(G)
+
     # Find all triangles
     triad_cliques = [x for x in all_cliques if len(x)==3 ]
-    print(f"\nThe number of triangles is: {len(triad_cliques)}")
 
+    print(f"The number of triangles is: {len(triad_cliques)}")
 
